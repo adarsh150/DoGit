c5ce475e2e5d46d0202c114eed4101c198723a26
adarsh150 <agrawal.adarsh3004@gmail.com>
2020-01-16 14:53:31 +0530

    Feature1 Experiment1 	Doing EXperiment with Branching 	This is branch Created Based On master branch

--- Code/hello.c
+++ Code/hello.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+void doBranchingExperiment1();
 int main()
 {
 	int doSomething = 100;
@@ -6,4 +7,7 @@ int main()
 	printf("fhg\n");
 }
 void doSomething(){
+}
+
+void doBranchingExperiment1(){
 }
